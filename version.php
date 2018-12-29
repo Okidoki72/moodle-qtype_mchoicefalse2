9c0160ec49ccd957f08caaa5d1c400e0615c62b0
Fabien Cornillier <fabienco@gmail.com>
2018-12-28 20:48:32 -0500

    Update version.php
    Set version to 2018122801

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_mchoiceftwo';
-$plugin->version   = 2018122800;
+$plugin->version   = 2018122801;
 
 $plugin->requires  = 2015111600;
 
